fd31aa69ec7b939e00f85744955cd7500afc16c9
MauricioAlonso <program.alonso@gmail.com>
2024-04-16 00:59:38 -0300

    Listo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "semantic-ui-react";
 
-function App() {
+export default function app() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+      <div>
+        <h1>Estamos en mi web</h1>
+      </div>
+    </>
   );
 }
-
-export default App;
