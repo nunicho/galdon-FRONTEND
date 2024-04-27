3f806020356ec44f1edde3d5e44867c91a16a676
MauricioAlonso <program.alonso@gmail.com>
2024-04-27 20:42:27 -0300

    avanzando

--- src/components/Admin/Auth/LoginForm/LoginForm.js
+++ src/components/Admin/Auth/LoginForm/LoginForm.js
@@ -1,3 +1,63 @@
+import React, { useState } from "react";
+import { Form} from "semantic-ui-react";
+import { useFormik } from "formik";
+import { Auth } from "../../../../api";
+import { useAuth } from "../../../../hooks";
+import { initialValues, validationSchema } from "./LoginForm.form";
+import "./LoginForm.scss";
+
+const authController = new Auth();
+
+export function LoginForm() {
+  const { login } = useAuth();
+  const [error, setError] = useState("")
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: validationSchema,
+    onSubmit: async (formValue) => {
+      try {
+        const response = await authController.login(formValue);
+
+        authController.setAccesToken(response.access);
+        authController.setRefreshToken(response.refresh);
+
+        login(response.access);
+      } catch (error) {
+        console.error("el error es ", error);
+        setError(error)
+      }     
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="email"
+        placeholder="Correo electrónico"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        error={formik.errors.password}
+      />
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
+        Entrar
+      </Form.Button>
+      {error && error.msg && (
+        <p className="login-form__error">{error.msg}</p>
+      )}
+    </Form>
+  );
+}
+
+/*
 import React from "react";
 import { Form } from "semantic-ui-react";
 import { useFormik } from "formik";
@@ -51,60 +111,8 @@ export function LoginForm() {
     </Form>
   );
 }
-/*
-import React from "react";
-import { Form } from "semantic-ui-react";
-import { useFormik } from "formik";
-import { Auth } from "../../../../api";
-import {useAuth} from "../../../../hooks"
-import { initialValues, validationSchema } from "./LoginForm.form";
-
-const authController = new Auth();
-
-export function LoginForm() {
-
-
-  const {login} = useAuth()
-
-  const formik = useFormik({
-    initialValues: initialValues,
-    validationSchema: validationSchema,
-    validateOnChange: false,
-    onSubmit: async (formValue) => {
-      try {
-        const response = await authController.login(formValue);
-
-        authController.setAccesToken(response.access)
-        authController.setRefreshToken(response.refresh)
-
-        login(response.access)
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  });
-
-  return (
-    <Form onSubmit={formik.handleSubmit}>
-      <Form.Input
-        name="email"
-        placeholder="Correo electrónico"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-        error={formik.errors.email}
-      />
-      <Form.Input
-        name="password"
-        type="password"
-        placeholder="Contraseña"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-        error={formik.errors.password}
-      />
-      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
-        Entrar
-      </Form.Button>
-    </Form>
-  );
-}
 */
+
+
+
+
--- src/components/Admin/Auth/RegisterForm/RegisterForm.js
+++ src/components/Admin/Auth/RegisterForm/RegisterForm.js
@@ -1,6 +1,83 @@
 import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
 import { useFormik } from "formik";
+import { Auth } from "../../../../api";
+import { initialValues, validationSchema } from "./RegisterForm.form";
+import "./RegisterForm.scss";
+
+const authController = new Auth();
+
+export function RegisterForm(props) {
+  const { openLogin } = props;
+  const [error, setError] = useState("");
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      try {
+        setError("");
+        await authController.register(formValue);
+        openLogin();
+      } catch (error) {
+        console.error("Error:", error);
+        if (error) {
+          setError(error.msg);
+        } else {
+          setError("Error en el servidor");
+        }
+      }
+    },
+  });
+
+  return (
+    <Form className="register-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="email"
+        placeholder="Correo Electrónico"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        error={formik.errors.password}
+      />
+      <Form.Input
+        name="repeatPassword"
+        type="password"
+        placeholder="Repetir contraseña"
+        onChange={formik.handleChange}
+        value={formik.values.repeatPassword}
+        error={formik.errors.repeatPassword}
+      />
+      <Form.Checkbox
+        name="conditionsAccepted"
+        label="He leído y acepto las políticas de privacidad"
+        onChange={(_, data) =>
+          formik.setFieldValue("conditionsAccepted", data.checked)
+        }
+        checked={formik.values.conditionsAccepted}
+        error={formik.errors.conditionsAccepted}
+      />
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
+        Crear Cuenta
+      </Form.Button>
+
+      {error && <p className="register-form__error">{error}</p>}
+    </Form>
+  );
+}
+
+/*
+import React, { useState } from "react";
+import { Form } from "semantic-ui-react";
+import { useFormik } from "formik";
 import {Auth} from "../../../../api"
 import { initialValues, validationSchema } from "./RegisterForm.form";
 import "./RegisterForm.scss";
@@ -68,3 +145,6 @@ const {openLogin} = props
     </Form>
   );
 }
+
+
+*/
